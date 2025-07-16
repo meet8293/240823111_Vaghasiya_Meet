acd313f61e171b8d4483e3cff0649dd3af0a8682
Meet <meetvaghasiya64@gmail.com>
2025-07-16 15:34:37 +0530

    Meet

---
+++ Unit=1/index.js
@@ -0,0 +1,90 @@
+// const name = "Vaghasiya Meet"
+// console.log("Hello "+name)
+// console.log(`Hello ${name}`)
+
+// (1).Check Even or Odd
+// Definition: Determine if a number is even or odd.
+
+// console.log("Check Even or Odd")
+// const readline = require('node:readline');
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+// rl.question("Enter the number: ", n => {
+//     // console.log(n);
+//     // console.log(typeof(n));
+//     if(n % 2)
+//     {
+//         console.log("number is odd")
+//     }
+//     else
+//     {
+//         console.log("number is even")
+//     }
+//     rl.close();
+// });
+
+
+
+
+// (2).Find the Maximum of Two Numbers
+// Definition: Given two numbers, print the larger one.
+
+// console.log("Find the Maximum of Two Numbers")
+// const readline = require('node:readline');
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+// rl.question("Enter the number: ", n1 => {
+// rl.question("Enter the number: ", n2 => {
+//     const a = parseFloat(n1);
+//     const b = parseFloat(n2);
+//     // console.log(n);
+//     // console.log(typeof(n));
+//     if(a > b)
+//     {
+//         console.log("a is max")
+//     }
+//     else(b > a)
+//     {
+//         console.log("b is max")
+//     }
+//     rl.close();
+// });
+// });
+
+
+
+
+// (3). Check Leap Year
+// Definition: Check if a given year is a leap year.
+
+// console.log("Check Leap Year")
+// const readline = require('node:readline');
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+// rl.question("Enter the year: ", n => {
+//     // console.log(n);
+//     // console.log(typeof(n));
+//     if(n % 4 == 0)
+//     {
+//         console.log("leap year")
+//     }
+//     else
+//     {
+//         console.log("not leap year")
+//     }
+//     rl.close();
+// });
+
+
+
+
+// (4). Sum of Natural Numbers
+// Definition: Calculate the sum of the first n natural numbers.
+
+// console.log("Sum of Natural Numbers")
