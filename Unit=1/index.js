4b9e48fbc2e4c707c7cc8a708e14c4085b7861f2
Meet <meetvaghasiya64@gmail.com>
2025-07-21 09:25:58 +0530

    Meet

--- Unit=1/index.js
+++ Unit=1/index.js
@@ -1,17 +1,21 @@
+// Write simple program Your name display.
 // const name = "Vaghasiya Meet"
 // console.log("Hello "+name)
 // console.log(`Hello ${name}`)
+
+
+
 
 // (1).Check Even or Odd
 // Definition: Determine if a number is even or odd.
 
 // console.log("Check Even or Odd")
 // const readline = require('node:readline');
-// const rl = readline.createInterface({
+// const eo = readline.createInterface({
 //   input: process.stdin,
 //   output: process.stdout,
 // });
-// rl.question("Enter the number: ", n => {
+// eo.question("Enter the number: ", n => {
 //     // console.log(n);
 //     // console.log(typeof(n));
 //     if(n % 2)
@@ -22,7 +26,7 @@
 //     {
 //         console.log("number is even")
 //     }
-//     rl.close();
+//     eo.close();
 // });
 
 
@@ -33,12 +37,12 @@
 
 // console.log("Find the Maximum of Two Numbers")
 // const readline = require('node:readline');
-// const rl = readline.createInterface({
+// const m = readline.createInterface({
 //   input: process.stdin,
 //   output: process.stdout,
 // });
-// rl.question("Enter the number: ", n1 => {
-// rl.question("Enter the number: ", n2 => {
+// m.question("Enter the number: ", n1 => {
+//     m.question("Enter the number: ", n2 => {
 //     const a = parseFloat(n1);
 //     const b = parseFloat(n2);
 //     // console.log(n);
@@ -51,23 +55,23 @@
 //     {
 //         console.log("b is max")
 //     }
-//     rl.close();
+//     m.close();
 // });
 // });
 
 
 
 
-// (3). Check Leap Year
+// (3).Check Leap Year
 // Definition: Check if a given year is a leap year.
 
 // console.log("Check Leap Year")
 // const readline = require('node:readline');
-// const rl = readline.createInterface({
+// const leap = readline.createInterface({
 //   input: process.stdin,
 //   output: process.stdout,
 // });
-// rl.question("Enter the year: ", n => {
+// leap.question("Enter the year: ", n => {
 //     // console.log(n);
 //     // console.log(typeof(n));
 //     if(n % 4 == 0)
@@ -78,13 +82,98 @@
 //     {
 //         console.log("not leap year")
 //     }
-//     rl.close();
+//     leap.close();
 // });
 
 
 
 
-// (4). Sum of Natural Numbers
+// (4).Sum of Natural Numbers
 // Definition: Calculate the sum of the first n natural numbers.
 
 // console.log("Sum of Natural Numbers")
+// const readline = require('node:readline');
+// const s = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+// s.question("Enter the number: ", n => {
+//     // n = parseInt(n); 
+//     let sum = 0;    
+//     for (let i = 1; i <= n; i++) 
+//     {
+//         sum += i;   
+//     }
+//     console.log(`Sum of natural numbers is: ${sum}`);
+// s.close();
+// });
+
+
+
+
+// (5).Factorial of a Number
+// Definition: Calculate factorial of a number.
+
+// console.log("Factorial of a Number")
+// const readline = require('node:readline');
+// const f = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+// f.question("Enter the number: ", n => {
+//     n = parseInt(n); 
+//     let fact = 1;
+//     for (let i = 1; i <= n; i++) 
+//     {
+//         fact *= i;   
+//     }
+//     console.log(`Factorial numbers is: ${fact}`);
+// f.close();
+// });
+
+
+
+
+// (6).Print Multiplication Table 
+// Definition: Display multiplication table of a number up to 10.
+
+// console.log("Multiplication Table")
+// const readline = require('node:readline');
+// const m = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+// m.question("Enter the number: ", n => {
+//     n = parseInt(n); 
+//     for (let i = 1; i <= 10; i++) 
+//     {
+//         console.log(`${n} x ${i} = ${n * i}`);
+//     }
+// m.close();
+// });
+
+
+
+
+// (7).Reverse a Number 
+// Definition: Reverse the digits of a number.
+
+// console.log("Reverse a Number")
+// const readline = require('node:readline');
+// const r = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+// r.question("Enter the number: ", n => {
+//     n = parseInt(n); 
+//     let original = n;
+//     let reversed = 0;
+//     while (n > 0)
+//     {
+//         let digit = n % 10;  
+//         reversed = reversed * 10 + digit;
+//         n = Math.floor(n / 10);
+//     }
+//     console.log(`Reverse number is: ${reversed}`);
+// r.close();
+// });
