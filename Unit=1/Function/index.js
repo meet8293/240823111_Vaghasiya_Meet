cc85ad02ae42a13f7dc8a789eb2661858d284ddc
Meet <meetvaghasiya64@gmail.com>
2025-07-22 11:19:21 +0530

    meet

---
+++ Unit=1/Function/index.js
@@ -0,0 +1,46 @@
+// import add from "./add.js";
+// // const add = require("./add.js");
+// console.log(add(11,22));
+// console.log(add(33,22));
+
+
+
+
+// (1).Path
+
+// const path = require("path");
+// console.log("Directory name: ",__dirname);
+// console.log(path.basename(__dirname + "/myfile.txt"));
+// console.log("Full path: ",path.resolve(__dirname,"myfile.txt"));
+// const moment = require("moment");
+
+
+
+
+// (2).moment
+
+// const moment =require("moment");
+// console.log("Current date and time: ",moment().format("DD-MM-YYYY HH:mm:ss"));
+
+
+
+
+//(3).lodash
+
+// const ls = require("lodash");
+// console.log("capitalize: ",ls.capitalize("Hello World"));
+
+
+
+
+// (4).fs
+
+// const fs = require("fs");
+// fs.writeFile("best.txt","Hello, MCA-Z2 Class!",(err)=>{
+// if(err) throw err;
+// console.log("File has been saved!");
+// });
+// fs.readFile("best.txt","utf8",(err,data)=>{
+// if(err) throw err;
+// console.log("File content: ",data);
+// });
