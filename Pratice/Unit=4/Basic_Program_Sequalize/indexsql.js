95f22dc39f70cddf0ede416e4286a0550f452104
Meet <meetvaghasiya64@gmail.com>
2025-09-17 13:16:41 +0530

    Meet

---
+++ Pratice/Unit=4/Basic_Program_Sequalize/indexsql.js
@@ -0,0 +1,102 @@
+//Sequelize
+const Sequelize = require("sequelize");
+
+//Connection
+const db = new Sequelize("mca", "root", "",{
+    host: "localhost",
+    dialect: "mysql"
+});
+
+//Test connection
+// db.authenticate()
+//     .then(() => {
+//         console.log("Connection has been established Successfully");
+//     })
+//     .catch((err) => {
+//          console.error("Unable to connect to the database: ",err);
+//     });
+ 
+//Create model   
+const Student = db.define("Student", {
+    id:{
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    city:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    }
+}) ;
+
+//Sync model
+// Student.sync()
+//     .then(() => {
+//         console.log("Student table created Successfully");
+//     })
+//     .catch((err) => {
+//         console.error("Unable to create table: ",err);
+//     });
+
+//insert data
+// Student.create({
+//     name: "Vaghasiya Yash V.",
+//     city: "Surat",
+//     email: "yashvaghasiya@gmail.com"
+// })
+//     .then(() => {
+//         console.log("Data inserted Successfully");
+//     })
+//     .catch((err) => {
+//         console.error("Unable to Insert data: ",err);
+//   });
+
+//fetch data
+// Student.findAll(where = {city: 'Surat'})
+//     .then((Students) => {
+//         console.log(Students);
+//     })
+//     .catch((err) => {
+//         console.error("Unable to Insert data: ",err);
+//     });
+
+//fetch single data
+// Student.findByPk(2)
+//     .then((Student) => {
+//         console.log(Student);
+//     })
+//     .catch((err) => {
+//         console.error("Unable to insert data: ",err);
+//     });
+
+//update data
+// Student.update({name: "Yash", city: "Gondal"},
+//     {
+//         where: {id: 2},
+//     })
+//     .then(() => {
+//         console.log("Data updated Successfully");
+//     })
+//     .catch((err) => {
+//         console.error("Unable to update data: ",err);
+//     });
+
+//delete data
+// Student.destroy({
+//     where: {id: 2},
+//     })
+//     .then(() => {
+//         console.log("Data deleted Successfully");
+//     })
+//     .catch((err) => {
+//         console.error("Unable to delete data: ",err);
+//     });
